b1063a03a14bfd14fb10a15a19f2fca9294acb95
Amrkahla6 <amr.elsaeed@almotahidaeducation.com>
2022-09-05 08:06:55 +0200

    Single Principle

--- classes/Match.php
+++ classes/Match.php
@@ -1,16 +1,21 @@
 <?php
 
+namespace classes;
+
 class Match{
 
-    public function attack(){
+    protected $attack;
+    protected $defense;
+    protected $keeper;
 
+    public function __construct()
+    {
+        $this->attack  = new Attack();
+        $this->defense = new Defense();
+        $this->keeper  = new Keeper();
     }
 
-    public function defence(){
-
-    }
-
-    public function keeper(){
-
+    public function start(){
+        echo "Start Play";
     }
 }
